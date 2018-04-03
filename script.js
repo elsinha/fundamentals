992f9c218e4223bb3fdb8ffe6da2e430065e542b
Elsinha <35178523+elsinha@users.noreply.github.com>
2018-04-03 10:59:18 -0700

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-// the variable is a container where we are going to store data typed by the viewer
-var name = prompt("What is your name?");
-alert("Hello, " + name);
+// first test the alert box
+alert("Hello World");
